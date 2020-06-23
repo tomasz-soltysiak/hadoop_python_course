bff1418e70721f1b5f30df9dfabf8691a1f94ad8
tomasz-soltysiak <49415314+tomasz-soltysiak@users.noreply.github.com>
2020-06-23 11:07:44 +0200

    Creating file in which I will implement simple map reduce

--- 01_simple_map_reduce.py
+++ 01_simple_map_reduce.py
@@ -1,1 +1,12 @@
 from mrjob.job import MRJob
+
+class MRWordCount(MRJob):
+    def mapper(self,_,line):
+        yield 'chars',len(line)
+
+    def reduce(self,key,values):
+        yield key, values
+
+
+if __name__ == '__main__':
+    MRWordCount.run()
