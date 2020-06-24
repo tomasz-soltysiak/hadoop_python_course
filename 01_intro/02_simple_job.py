2398ed1318e7669700bf74d2d6f9b93df5b208a2
tomasz-soltysiak <49415314+tomasz-soltysiak@users.noreply.github.com>
2020-06-24 14:49:50 +0200

    Another push

--- 01_intro/02_simple_job.py
+++ 01_intro/02_simple_job.py
@@ -1,6 +1,13 @@
 from mrjob.job import MRJob
+from mrjob.step import  MRStep
 
 class MRSimpleJob(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer)
+        ]
+
     def mapper(self,_,value):
         yield 'line',1
         yield 'words', len(value.split())
