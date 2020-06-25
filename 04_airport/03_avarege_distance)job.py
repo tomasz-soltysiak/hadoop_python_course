534d793eaad266066bdf02845b5da78ae8c63c71
tomasz-soltysiak <49415314+tomasz-soltysiak@users.noreply.github.com>
2020-06-25 15:57:49 +0200

    Another push

---
+++ 04_airport/03_avarege_distance)job.py
@@ -0,0 +1,30 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import numpy as np
+import re
+class MRFligths(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer)
+        ]
+    def mapper(self, _, line):
+        year,items=line.split('\t')
+        year=(year[1:-1])
+        items=items[1:-1]
+        month,day,airline,distance=items.split(', ')
+        distance=int(distance)
+        yield year,distance
+
+    def reducer(self, key, values):
+        num=0
+        val=0
+        for value in values:
+            num=num+1
+            val=val+value
+        yield None,val/num
+
+
+
+if __name__ == '__main__':
+    MRFligths.run()
