cc1e9f9cf5907627c3db0cf14223b5dc120c32f8
tomasz-soltysiak <49415314+tomasz-soltysiak@users.noreply.github.com>
2020-06-26 14:21:40 +0200

    Another push

--- 04_airport/05_plotly.py
+++ 04_airport/05_plotly.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 df=pd.read_csv('avg_delay_arrival.csv',sep='\t',names=['month','to_split'])
 df[['departure_delay','arrival_delay']]=df.to_split.str[1:-1].str.split(', ').apply(pd.Series)
@@ -7,4 +8,4 @@ df=df.drop(['to_split'],axis=1)
 #print(df.head())
 
 fig=px.bar(df,x='month',y='departure_delay',color='month')
-fig.show()
+    fig=go.Figure(data=[go.Bar(x=df.month,y=df.departute_delay),go.Bar(x=df.month,y=df.arrival_delay)])
